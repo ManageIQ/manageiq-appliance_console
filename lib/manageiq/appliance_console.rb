2e95e5ee5a6097d306c527c327a8e450ab8fae9d
Brandon Dunne <bdunne@redhat.com>
2017-10-17 14:00:12 -0400

    Move locales to the root

--- lib/manageiq/appliance_console.rb
+++ lib/manageiq/appliance_console.rb
@@ -5,6 +5,7 @@
 require 'manageiq-appliance_console'
 require 'pathname'
 
+gem_root = Pathname.new(__dir__).join("..")
 # Set up Environment
 ENV['BUNDLE_GEMFILE'] ||= ManageIQ::ApplianceConsole::RAILS_ROOT.join("Gemfile").to_s
 require 'bundler'
@@ -23,7 +24,7 @@ include HighLine::SystemExtensions
 require 'i18n'
 locales_dir = ENV['CONTAINER'] ? "container" : "appliance"
 locales_paths = [
-  File.expand_path(File.join("appliance_console/locales", locales_dir, "*.yml"), __dir__),
+  gem_root.join("locales", locales_dir, "*.yml"),
   File.expand_path(File.join("productization/appliance_console/locales", locales_dir, "*.yml"), ManageIQ::ApplianceConsole::RAILS_ROOT)
 ]
 locales_paths.each { |p| I18n.load_path += Dir[p].sort }
