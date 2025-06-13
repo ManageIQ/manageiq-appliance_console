cb016e0c4f05f0b8f76895f0ba5de0f519aa096d
Adam Grare <adam@grare.com>
2025-06-13 12:45:40 -0400

    Add options to authenticate to a container registry

--- lib/manageiq/appliance_console/containers_configuration.rb
+++ lib/manageiq/appliance_console/containers_configuration.rb
@@ -9,32 +9,53 @@ module ManageIQ
       CONTAINERS_ROOT_DIR = Pathname.new("/var/lib/manageiq/containers").freeze
       CONTAINERS_VOL_NAME = "miq_containers".freeze
 
-      attr_accessor :disk
+      attr_accessor :registry_uri, :registry_username, :registry_password,
+                    :registry_authfile, :registry_certdir, :registry_tls_verify, :disk
 
       def initialize(options = {})
-        self.disk = options[:disk]
+        self.registry_uri      = options[:container_registry_uri]
+        self.registry_username = options[:container_registry_username]
+        self.registry_password = options[:container_registry_password]
+        self.registry_authfile = options[:container_registry_authfile]
+        self.disk              = options[:disk]
       end
 
       def ask_questions
         clear_screen
-        choose_disk if use_new_disk
+        choose_disk if use_new_disk?
+        choose_container_registry if authenticate_container_registry?
         confirm_selection
       end
 
       def activate
-        return true unless disk
+        if disk
+          say("Initializing container storage disk")
 
-        say("Initializing container storage disk")
+          FileUtils.mkdir_p(CONTAINERS_ROOT_DIR)
+          LogicalVolumeManagement.new(:disk => disk, :mount_point => CONTAINERS_ROOT_DIR, :name => CONTAINERS_VOL_NAME).setup
+          FileUtils.chown(manageiq_uid, manageiq_gid, CONTAINERS_ROOT_DIR)
+        end
 
-        FileUtils.mkdir_p(CONTAINERS_ROOT_DIR)
-        LogicalVolumeManagement.new(:disk => disk, :mount_point => CONTAINERS_ROOT_DIR, :name => CONTAINERS_VOL_NAME).setup
-        FileUtils.chown(manageiq_uid, manageiq_gid, CONTAINERS_ROOT_DIR)
+        if registry_uri
+          say("Authenticating to container registry #{registry_uri}...")
+
+          login_params = {
+            :username   => registry_username,
+            :password   => registry_password,
+            :authfile   => registry_authfile,
+            :cert_dir   => registry_certdir,
+            :tls_verify => registry_tls_verify
+          }
+
+          podman!("login", registry_uri, login_params.compact)
+        end
+
         true
       end
 
       private
 
-      def use_new_disk
+      def use_new_disk?
         agree("Configure a new disk for container storage? (Y/N):")
       end
 
@@ -42,14 +63,37 @@ module ManageIQ
         self.disk = ask_for_disk("container disk")
       end
 
+      def authenticate_container_registry?
+        agree("Authenticate to a container registry? (Y/N):")
+      end
+
+      def choose_container_registry
+        self.registry_uri      = ask_for_string("Registry:")
+        self.registry_username = ask_for_string("Registry username:")
+        self.registry_password = ask_for_password("Registry password:")
+      end
+
       def confirm_selection
-        return false unless disk
+        return false unless disk || registry_uri
 
         clear_screen
 
-        say("\t#{disk.path} with #{disk.size.to_i / 1.gigabyte} GB will be configured as the new containers root disk.")
+        if disk
+          say("\t#{disk.path} with #{disk.size.to_i / 1.gigabyte} GB will be configured as the new containers root disk.")
+        end
+
+        if registry_uri
+          say("Authenticating to container registry #{registry_uri}")
+        end
 
         agree("Confirm continue with these updates (Y/N):")
+      end
+
+      def podman!(*args, **kwargs)
+        params = [{:u => "manageiq"}, "podman", {:root => CONTAINERS_ROOT_DIR.join("storage")}]
+        params.concat(args)
+
+        AwesomeSpawn.run!("sudo", :params => params, **kwargs)
       end
     end
   end
